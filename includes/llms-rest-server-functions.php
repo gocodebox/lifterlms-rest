9ab7f6d84dffc5fd232df52f7ca3575ff217617e
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-07-24 09:51:28 -0700

    comment cs

--- includes/llms-rest-server-functions.php
+++ includes/llms-rest-server-functions.php
@@ -11,7 +11,7 @@
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Return a WP_Error with proper code, message and status for unauthorized requets.
+ * Return a WP_Error with proper code, message and status for unauthorized requests.
  *
  * @since [version]
  *
@@ -19,11 +19,11 @@ defined( 'ABSPATH' ) || exit;
  */
 function llms_rest_authorization_required_error() {
 	if ( is_user_logged_in() ) {
-		// 403
+		// 403.
 		$error_code = 'llms_rest_forbidden_request';
 		$message    = __( 'You are not authorized to perform this request.', 'lifterlms' );
 	} else {
-		// 400
+		// 401.
 		$error_code = 'llms_rest_unauthorized_request';
 		$message    = __( 'The API credentials were invalid.', 'lifterlms' );
 	}
@@ -38,7 +38,7 @@ function llms_rest_authorization_required_error() {
  * @return WP_Error
  */
 function llms_rest_not_found_error() {
-	// 404
+	// 404.
 	return new WP_Error( 'llms_rest_not_found', __( 'The requested resource could not be found.', 'lifterlms' ), array( 'status' => 404 ) );
 }
 
@@ -50,6 +50,6 @@ function llms_rest_not_found_error() {
  * @return WP_Error
  */
 function llms_rest_bad_request_error() {
-	// 400
+	// 400.
 	return new WP_Error( 'llms_rest_bad_request', __( 'Invalid or malformed request syntax.', 'lifterlms' ), array( 'status' => 400 ) );
 }
