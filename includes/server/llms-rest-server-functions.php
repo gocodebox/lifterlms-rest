ef4d6fe8d5070b95f030ca3f5578049bd97875d3
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-08-08 18:12:50 -0700

    server function tests, new error message functions for 500 errors

--- includes/server/llms-rest-server-functions.php
+++ includes/server/llms-rest-server-functions.php
@@ -35,6 +35,20 @@ function llms_rest_authorization_required_error( $message = '' ) {
 }
 
 /**
+ * Return a WP_Error with proper code, message and status for invalid or malformed request syntax.
+ *
+ * @since [version]
+ *
+ * @param string $message Optional. The custom error message. Default empty string.
+ *                        When no custom message is provided a predefined message will be used.
+ * @return WP_Error
+ */
+function llms_rest_bad_request_error( $message = '' ) {
+	$message = ! $message ? __( 'Invalid or malformed request syntax.', 'lifterlms' ) : $message;
+	return new WP_Error( 'llms_rest_bad_request', $message, array( 'status' => 400 ) );
+}
+
+/**
  * Return a WP_Error with proper code, message and status for not found resources.
  *
  * @since [version]
@@ -49,15 +63,16 @@ function llms_rest_not_found_error( $message = '' ) {
 }
 
 /**
- * Return a WP_Error with proper code, message and status for invalid or malformed request syntax.
+ * Return a WP_Error for a 500 Internal Server Error.
  *
  * @since [version]
  *
- * @param string $message Optional. The custom error message. Default empty string.
- *                        When no custom message is provided a predefined message will be used.
+ * @param string $message Optional. Custom error message. When none provided a predefined message is used.
  * @return WP_Error
  */
-function llms_rest_bad_request_error( $message = '' ) {
-	$message = ! $message ? __( 'Invalid or malformed request syntax.', 'lifterlms' ) : $message;
-	return new WP_Error( 'llms_rest_bad_request', $message, array( 'status' => 400 ) );
+function llms_rest_server_error( $message = '' ) {
+	$message = ! $message ? __( 'Internal Server Error.', 'lifterlms' ) : $message;
+	return new WP_Error( 'llms_rest_server_error', $message, array( 'status' => 500 ) );
 }
+
+
