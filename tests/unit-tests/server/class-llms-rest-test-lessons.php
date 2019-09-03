abcecbdcd098cf05d42df7fec5a5d17b5c41048d
Rocco Aliberti <raliberti84@gmail.com>
2019-09-03 13:00:47 +0200

    Lessons: add some tests

---
+++ tests/unit-tests/server/class-llms-rest-test-lessons.php
@@ -0,0 +1,213 @@
+<?php
+/**
+ * Tests for Lessons API.
+ *
+ * @package LifterLMS_Rest/Tests/Controllers
+ *
+ * @group REST
+ * @group rest_lessons
+ *
+ * @since [version]
+ * @version [version]
+ */
+class LLMS_REST_Test_Lessons extends LLMS_REST_Unit_Test_Case_Posts {
+
+	/**
+	 * Route.
+	 *
+	 * @var string
+	 */
+	private $route = '/llms/v1/lessons';
+
+	/**
+	 * Post type.
+	 *
+	 * @var string
+	 */
+	protected $post_type = 'lessons';
+
+
+	/**
+	 * Array of link $rels expected for each item.
+	 *
+	 * @var array
+	 */
+	private $expected_link_rels = array( 'self', 'collection', 'course', 'parent', 'siblings', 'next', 'previous' );
+
+	/**
+	 * Setup our test server, endpoints, and user info.
+	 */
+	public function setUp() {
+
+		parent::setUp();
+		$this->user_allowed = $this->factory->user->create(
+			array(
+				'role' => 'administrator',
+			)
+		);
+
+		$this->user_forbidden = $this->factory->user->create(
+			array(
+				'role' => 'subscriber',
+			)
+		);
+
+		global $wpdb;
+		$wpdb->delete( $wpdb->prefix . 'posts', array( 'post_type' => $this->post_type ) );
+
+		$this->endpoint = new LLMS_REST_Lessons_Controller();
+	}
+
+
+	/**
+	 * Test route registration.
+	 *
+	 * @since [version]
+	 */
+	public function test_register_routes() {
+
+		$routes = $this->server->get_routes();
+		$this->assertArrayHasKey( $this->route, $routes );
+		$this->assertArrayHasKey( $this->route . '/(?P<id>[\d]+)', $routes );
+	}
+
+	/**
+	 * Test the item schema.
+	 *
+	 * @since [version]
+	 */
+	public function test_get_item_schema() {
+
+		$schema = $this->endpoint->get_item_schema();
+
+
+		$this->assertEquals( 'lesson', $schema['title'] );
+
+		$props = array(
+			'id',
+			'audio_embed',
+			'assignment',
+			'comment_status',
+			'content',
+			'course_id',
+			'date_created',
+			'date_created_gmt',
+			'date_updated',
+			'date_updated_gmt',
+			'drip_date',
+			'drip_days',
+			'drip_method',
+			'excerpt',
+			'featured_media',
+			'menu_order',
+			'order',
+			'parent_id',
+			'password',
+			'permalink',
+			'ping_status',
+			'points',
+			'post_type',
+			'prerequisite',
+			'public',
+			'quiz',
+			'slug',
+			'status',
+			'title',
+			'video_embed',
+		);
+
+		$schema_keys = array_keys( $schema['properties'] );
+		sort( $schema_keys );
+		sort( $props );
+
+		$this->assertEquals( $props, $schema_keys );
+
+		// check nested items.
+		$assignment_quiz_nested = array(
+			'enabled',
+			'id',
+			'progression'
+		);
+		$this->assertEquals( $assignment_quiz_nested, array_keys( $schema['properties']['assignment']['properties'] ) );
+		$this->assertEquals( $assignment_quiz_nested, array_keys( $schema['properties']['quiz']['properties'] ) );
+
+	}
+
+	// public function test_get_items_success() {}
+	// public function test_get_items_exclude() {}
+	// public function test_get_items_include() {}
+	// public function test_get_items_orderby_id() {}
+	// public function test_get_items_orderby_email() {}
+	// public function test_get_items_orderby_name() {}
+	// public function test_get_items_orderby_registered_date() {}
+	// public function test_get_items_pagination() {}
+	// public function test_get_items_filter_by_posts() {}
+	// public function test_get_items_filter_by_roles() {}
+
+	// public function test_create_item_success() {}
+	// public function test_create_item_missing_required() {}
+	// public function test_create_item_auth_errors() {}
+
+	// public function test_get_item_success() {}
+	// public function test_get_item_auth_errors() {}
+	// public function test_get_item_not_found() {}
+
+	// public function test_update_item_success() {}
+	// public function test_update_item_auth_errors() {}
+	// public function test_update_item_errors() {}
+
+
+	// public function test_delete_item_success() {}
+	// public function test_get_item_auth_errors() {}
+
+
+	/**
+	 * Test links.
+	 *
+	 * @since [version]
+	 */
+    public function test_links() {
+
+		// create course with 1 section and 3 lessons per section.
+		$course = $this->factory->course->create_and_get( array( 'sections' => 1, 'lessons' => 3 ) );
+
+		$course_obj  = new LLMS_Course( $course );
+		$lessons     = $course_obj->get_lessons();
+
+		wp_set_current_user( $this->user_allowed );
+
+		$i = 0;
+		foreach ( $lessons as $lesson ) {
+
+			/**
+			 * Latest leson: remove the quiz.
+			 */
+			if ( 3 === $i ) {
+				$lesson->set( 'quiz_enabled', 'no' );
+			}
+
+			$response = $this->perform_mock_request( 'GET',  $this->route . '/' . $lesson->get( 'id' )  );
+
+			switch ( $i++ ):
+				case 0:
+					$expected_link_rels = array_values( array_diff( $this->expected_link_rels, array( 'previous' ) ) );
+					break;
+				case 2:
+					$expected_link_rels = array_values( array_diff( $this->expected_link_rels, array( 'next' ) ) );
+					break;
+				default:
+					$expected_link_rels = $this->expected_link_rels;
+			endswitch;
+
+
+			if ( $lesson->is_quiz_enabled() ) {
+				$expected_link_rels[] = 'quiz';
+			}
+
+			$this->assertEquals( $expected_link_rels, array_keys( $response->get_links() ) );
+
+		}
+
+	}
+
+}
