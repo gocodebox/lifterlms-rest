a386a00896ea766dab93aa611e05394b74577286
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-02-04 16:02:27 -0800

    fix failing test resulting from tests upgrade

--- tests/unit-tests/class-llms-rest-test-webhooks-query.php
+++ tests/unit-tests/class-llms-rest-test-webhooks-query.php
@@ -8,7 +8,7 @@
  * @group webhooks_query
  *
  * @since 1.0.0-beta.1
- * @version 1.0.0-beta.1
+ * @since [version] Call parent method from setUp().
  */
 class LLMS_REST_Test_Webhooks_Query extends LLMS_REST_Unit_Test_Case_Base {
 
@@ -16,10 +16,13 @@ class LLMS_REST_Test_Webhooks_Query extends LLMS_REST_Unit_Test_Case_Base {
 	 * Setup test.
 	 *
 	 * @since 1.0.0-beta.1
+	 * @since [version] Call parent method.
 	 *
 	 * @return void
 	 */
 	public function setUp() {
+
+		parent::setUp();
 
 		global $wpdb;
 		$wpdb->query( "TRUNCATE TABLE {$wpdb->prefix}lifterlms_webhooks" );
