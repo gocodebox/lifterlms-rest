2dacaed5a5835776dadce00f2949d7fc4b015d8c
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-04-13 10:05:18 -0700

    build

--- spec/code_samples/Node.js/lessons/get.js
+++ spec/code_samples/Node.js/lessons/get.js
@@ -5,7 +5,7 @@ const llms = new llmsAPI( {
   "consumerSecret": "cs_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 } );
 
-llms.get( '/lessons?context=edit&page=1&per_page=SOME_INTEGER_VALUE&order=SOME_STRING_VALUE&orderby=SOME_STRING_VALUE&include=1%2C2%2C3&exclude=10%2C11%2C12&parent=987', function( err, data, res ) {
+llms.get( '/lessons?context=edit&page=1&per_page=SOME_INTEGER_VALUE&order=SOME_STRING_VALUE&orderby=SOME_STRING_VALUE&include=1%2C2%2C3&exclude=10%2C11%2C12&status=draft&parent=987', function( err, data, res ) {
   if ( err ) {
     throw new Error( 'Error!' );
   }
