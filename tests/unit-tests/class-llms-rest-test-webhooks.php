f33dc39c48667c726054fe144f19306e777f59b3
Brian Hogg <brian@bhconsulting.ca>
2025-03-31 08:21:31 -0400

    Changing URL. Probably shouldn't be using an actual network request here.

--- tests/unit-tests/class-llms-rest-test-webhooks.php
+++ tests/unit-tests/class-llms-rest-test-webhooks.php
@@ -52,7 +52,7 @@ class LLMS_REST_Test_Webhooks extends LLMS_REST_Unit_Test_Case_Base {
 
 		$ret = $this->webhooks->create( array(
 			'topic' => 'course.created',
-			'delivery_url' => 'https://mock.com',
+			'delivery_url' => 'https://sdlkfj.lifterlms.com',
 		) );
 
 		$this->assertIsWPError( $ret );
