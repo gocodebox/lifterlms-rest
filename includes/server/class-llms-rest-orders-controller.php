deba91adb384df3d3cce03f775e37487ed4c1785
Brian Hogg <brian@bhconsulting.ca>
2025-06-26 16:03:53 -0400

    WIP: Adding additional fields

--- includes/server/class-llms-rest-orders-controller.php
+++ includes/server/class-llms-rest-orders-controller.php
@@ -47,6 +47,30 @@ class LLMS_REST_Orders_Controller extends LLMS_REST_Posts_Controller {
 			'embeddable' => true,
 		);
 
+		$product = $object->get_product();
+		if ( ! $product ) {
+			return $links;
+		}
+
+		switch ( $product->get( 'type' ) ) {
+			case 'course':
+				$links['product'] = array(
+					'href'       => rest_url(
+						sprintf( '/%s/%s/%d', 'llms/v1', 'courses', $object->get( 'product_id' ) )
+					),
+					'embeddable' => true,
+				);
+				break;
+			case 'llms_membership':
+				$links['product'] = array(
+					'href'       => rest_url(
+						sprintf( '/%s/%s/%d', 'llms/v1', 'memberships', $object->get( 'product_id' ) )
+					),
+					'embeddable' => true,
+				);
+				break;
+		}
+
 		return $links;
 	}
 
@@ -63,8 +87,13 @@ class LLMS_REST_Orders_Controller extends LLMS_REST_Posts_Controller {
 
 		$data = parent::prepare_object_for_response( $order, $request );
 
-		$data['status']        = str_replace( 'llms-', '', $data['status'] );
-		$data['billing_email'] = $order->get( 'billing_email' );
+		$data['status']          = str_replace( 'llms-', '', $data['status'] );
+		$data['billing_email']   = $order->get( 'billing_email' );
+		$data['payment_gateway'] = $order->get( 'payment_gateway' );
+		$data['coupon_amount']   = $order->get( 'coupon_amount' );
+		$data['original_total']  = $order->get( 'original_total' );
+		$data['product_id']      = $order->get( 'product_id' );
+		$data['sale_value']      = $order->get( 'sale_value' );
 
 		return $data;
 	}
@@ -81,9 +110,34 @@ class LLMS_REST_Orders_Controller extends LLMS_REST_Posts_Controller {
 		$schema = (array) parent::get_item_schema_base();
 
 		$order_properties = array(
-			'billing_email' => array(
+			'billing_email'   => array(
 				'description' => __( 'Billing email address for the order.', 'lifterlms' ),
 				'type'        => 'string',
+				'context'     => array( 'view', 'edit', 'embed' ),
+			),
+			'payment_gateway' => array(
+				'description' => __( 'Payment gateway used for the order.', 'lifterlms' ),
+				'type'        => 'string',
+				'context'     => array( 'view', 'edit', 'embed' ),
+			),
+			'coupon_amount'   => array(
+				'description' => __( 'Total amount of any coupons applied to the order.', 'lifterlms' ),
+				'type'        => 'number',
+				'context'     => array( 'view', 'edit', 'embed' ),
+			),
+			'original_total'  => array(
+				'description' => __( 'Original total amount of the order before any discounts or coupons.', 'lifterlms' ),
+				'type'        => 'number',
+				'context'     => array( 'view', 'edit', 'embed' ),
+			),
+			'product_id'      => array(
+				'description' => __( 'ID of the product associated with the order.', 'lifterlms' ),
+				'type'        => 'integer',
+				'context'     => array( 'view', 'edit', 'embed' ),
+			),
+			'sale_value'      => array(
+				'description' => __( 'Sale value of the order.', 'lifterlms' ),
+				'type'        => 'number',
 				'context'     => array( 'view', 'edit', 'embed' ),
 			),
 		);
