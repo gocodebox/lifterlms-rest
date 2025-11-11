da28ff92bb993bc859b33a053721e7d36ff7cdb9
Brian Hogg <brian@bhconsulting.ca>
2025-11-11 09:27:30 -0500

    Release v1.0.3 [ci skip]

--- lifterlms-rest.php
+++ lifterlms-rest.php
@@ -10,7 +10,7 @@
  * Plugin Name: LifterLMS REST API
  * Plugin URI: https://lifterlms.com/
  * Description: REST API feature plugin for the LifterLMS Core.
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: LifterLMS
  * Author URI: https://lifterlms.com/
  * Text Domain: lifterlms
